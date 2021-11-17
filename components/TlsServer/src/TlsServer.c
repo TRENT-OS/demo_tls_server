eae6a50869b548ca676e9915a1c7dfe0b56e3602
Franz Schauer <franz.schauer@hensoldt-cyber.de>
2021-11-17 16:49:46 +0100

    [DEV] Replace mbedtls with os_tls
    Replace the *raw* mbedtls usage by using os_tls_server.
    Since it is now not (easily) possible to retrieve the chosen ciphersuite anymore, the HTTP response of the demo was changed to a simple "Hello TLS!" webpage.

--- components/TlsServer/src/TlsServer.c
+++ components/TlsServer/src/TlsServer.c
@@ -15,8 +15,10 @@
 
 #include "interfaces/if_OS_Entropy.h"
 
+#include "OS_Crypto.h"
 #include "OS_Error.h"
 #include "OS_Socket.h"
+#include "OS_Tls.h"
 
 #include "mbedtls/certs.h"
 #include "mbedtls/ctr_drbg.h"
@@ -33,27 +35,9 @@
 #define HTTP_RESPONSE \
     "HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n" \
     "<h2>mbed TLS Test Server</h2>\r\n" \
-    "<p>Successful connection using: %s</p>\r\n"
+    "<p>Successful connection: Hello TLS!</p>\r\n"
 
 //------------------------------------------------------------------------------
-
-static const char cTlsServerRootCert[] = TLS_SERVER_ROOT_CERT;
-static const size_t cTlsServerRootCertLen = sizeof( cTlsServerRootCert );
-
-static const char cTlsServerCert[] = TLS_SERVER_CERT;
-static const size_t cTlsServerCertLen = sizeof( cTlsServerCert );
-
-static const char cTlsServerKey[] = TLS_SERVER_KEY;
-static const size_t cTlsServerKeyLen = sizeof( cTlsServerKey );
-
-static const int cCipherSuites[] =
-{
-    MBEDTLS_TLS_DHE_RSA_WITH_AES_128_GCM_SHA256,
-    MBEDTLS_TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-    MBEDTLS_TLS_RSA_WITH_AES_128_CBC_SHA,
-
-    0 // list needs to be 0 terminated
-};
 
 static const if_OS_Socket_t networkStackCtx =
     IF_OS_SOCKET_ASSIGN(networkStack);
@@ -77,7 +61,8 @@ waitForNetworkStackInit(
         else if (networkStackState == FATAL_ERROR)
         {
             // NetworkStack will not come up.
-            Debug_LOG_ERROR("A FATAL_ERROR occurred in the Network Stack component.");
+            Debug_LOG_ERROR("A FATAL_ERROR occurred in the Network Stack "
+                            "component.");
             return OS_ERROR_ABORTED;
         }
 
@@ -120,8 +105,8 @@ waitForIncomingConnection(
 
         if (numberOfSocketsWithEvents == 0)
         {
-            Debug_LOG_TRACE("OS_Socket_getPendingEvents() returned "
-                            "without any pending events");
+            Debug_LOG_TRACE("OS_Socket_getPendingEvents() returned without any "
+                            "pending events");
             continue;
         }
 
@@ -189,174 +174,24 @@ waitForIncomingConnection(
     return ret;
 }
 
-static int
-sendFunc(
-    void* ctx,
-    const unsigned char* buf,
-    size_t len)
-{
-    OS_Socket_Handle_t* socket = (OS_Socket_Handle_t*)ctx;
-    size_t n = len > MAX_NW_SIZE ? MAX_NW_SIZE : len;
-
-    OS_Error_t err = OS_Socket_write(*socket, buf, n, &n);
-
-    switch (err)
-    {
-    case OS_SUCCESS:
-        Debug_LOG_INFO(
-            "OS_Socket_write() sent %zu bytes of data", n);
-        break;
-
-    case OS_ERROR_TRY_AGAIN:
-        Debug_LOG_TRACE(
-            "OS_Socket_write() reported try again");
-        n = MBEDTLS_ERR_SSL_WANT_WRITE;
-        break;
-
-    default:
-        Debug_LOG_ERROR("OS_Socket_write() failed, error %d", err);
-        n = -1;
-        break;
-    }
-
-    return n;
-}
-
-static int
-recvFunc(
-    void* ctx,
-    unsigned char* buf,
-    size_t len)
-{
-    OS_Socket_Handle_t* socket = (OS_Socket_Handle_t*)ctx;
-    size_t n = len > MAX_NW_SIZE ? MAX_NW_SIZE : len;
-
-    OS_Error_t err = OS_Socket_read(*socket, buf, n, &n);
-
-    switch (err)
-    {
-    case OS_SUCCESS:
-        Debug_LOG_INFO(
-            "OS_Socket_read() received %zu bytes of data", n);
-        break;
-
-    case OS_ERROR_TRY_AGAIN:
-        Debug_LOG_TRACE(
-            "OS_Socket_read() reported try again");
-        n = MBEDTLS_ERR_SSL_WANT_READ;
-        break;
-
-    case OS_ERROR_CONNECTION_CLOSED:
-        Debug_LOG_INFO(
-            "OS_Socket_read() reported connection closed");
-        n = -1;
-        break;
-
-    case OS_ERROR_NETWORK_CONN_SHUTDOWN:
-        Debug_LOG_INFO(
-            "OS_Socket_read() reported connection shutdown");
-        n = -1;
-        break;
-
-    default:
-        Debug_LOG_ERROR(
-            "OS_Socket_read() failed, error %d", err);
-        n = -1;
-        break;
-    }
-
-    return n;
-}
-
-// This function is called by mbedTLS to log messages.
-static void
-logDebug(
-    void*       ctx,
-    int         level,
-    const char* file,
-    int         line,
-    const char* str)
-{
-    char msg[256];
-    UNUSED_VAR(ctx);
-    UNUSED_VAR(level);
-
-    if (level < 1 || level > 4)
-    {
-        return;
-    }
-
-    size_t len_file_name = strlen(file);
-    const size_t max_len_file = 16;
-    bool is_short_file_name = (len_file_name <= max_len_file);
-
-    snprintf(
-        msg,
-        sizeof(msg),
-        "[%s%s:%05i]: %s",
-        is_short_file_name ? "" : "...",
-        &file[ is_short_file_name ?  0 : len_file_name - max_len_file],
-        line,
-        str);
-
-    // Replace '\n' in mbedtls string with '\0' because otherwise it would
-    // result in extra empty lines because Debug_LOG_XXX adds another '\n'.
-    size_t msg_len = strlen(msg);
-    if (msg_len > 0 && msg[msg_len - 1] == '\n')
-    {
-        msg[msg_len - 1] = '\0';
-    }
-
-    // Translate mbedTLS level to appropriate debug output
-    switch (level)
-    {
-    case 1: // Error
-        Debug_LOG_ERROR("%s", msg);
-        break;
-    case 2: // State change
-    case 3: // Informational
-        Debug_LOG_INFO("%s", msg);
-        break;
-    case 4: // Verbose
-        Debug_LOG_DEBUG("%s", msg);
-        break;
-    default:
-        // Do nothing
-        break;
-    }
-}
-
-static int
-entropyWrapper(
-    void*          ctx,
-    unsigned char* buf,
-    size_t         len)
-{
-    if_OS_Entropy_t* entropy = (if_OS_Entropy_t*)ctx;
-
-    size_t s = entropy->read(len);
-    memcpy(buf, OS_Dataport_getBuf(entropy->dataport), s);
-
-    // If we cannot return as many bytes as requested, produce an error
-    return (s == len) ? 0 : -1;
-}
-
 //------------------------------------------------------------------------------
 
 int
 run(void)
 {
-    Debug_LOG_INFO("Starting TLS Server");
+    Debug_LOG_INFO("Starting TLS Server...");
 
     // Check and wait until the NetworkStack component is up and running.
     OS_Error_t err = waitForNetworkStackInit(&networkStackCtx);
     if (OS_SUCCESS != err)
     {
-        Debug_LOG_ERROR("waitForNetworkStackInit() failed with: %d", err);
+        Debug_LOG_ERROR("waitForNetworkStackInit() failed, code %d", err);
         return -1;
     }
 
     OS_Socket_Handle_t hServer;
+    OS_Socket_Handle_t hSocket;
+
     err = OS_Socket_create(
               &networkStackCtx,
               &hServer,
@@ -395,135 +230,63 @@ run(void)
     }
 
     // -------------------------------------------------------------------------
-    // Init mbedtls
 
-    mbedtls_ctr_drbg_context ctr_drbg;
-    mbedtls_ctr_drbg_init( &ctr_drbg );
+    OS_Crypto_Config_t cryptoCfg =
+    {
+        .mode = OS_Crypto_MODE_LIBRARY,
+        .entropy = IF_OS_ENTROPY_ASSIGN(
+            entropy_rpc,
+            entropy_port),
+    };
 
-    mbedtls_ssl_context ssl;
-    mbedtls_ssl_init( &ssl );
+    OS_Tls_Config_t tlsConfig =
+    {
+        .mode = OS_Tls_MODE_LIBRARY,
+        .library = {
+            .socket = {
+                .context    = &hSocket,
+            },
+            .flags = OS_Tls_FLAG_NONE,
+            .crypto = {
+                .policy     = NULL,
+                .caCerts    = TLS_SERVER_ROOT_CERT,
+                .ownCert    = TLS_SERVER_CERT,
+                .privateKey = TLS_SERVER_KEY,
+                .cipherSuites =
+                OS_Tls_CIPHERSUITE_FLAGS(
+                    OS_Tls_CIPHERSUITE_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+                    OS_Tls_CIPHERSUITE_DHE_RSA_WITH_AES_128_GCM_SHA256)
+            }
+        }
+    };
 
-    mbedtls_ssl_config conf;
-    mbedtls_ssl_config_init( &conf );
+    err = OS_Crypto_init(&tlsConfig.library.crypto.handle, &cryptoCfg);
+    if (OS_SUCCESS != err)
+    {
+        Debug_LOG_ERROR("OS_Crypto_init() failed, code %d", err);
+        return -1;
+    }
+    Debug_LOG_INFO("Crypto library successfully initialized");
 
-    mbedtls_x509_crt srvcert;
-    mbedtls_x509_crt_init( &srvcert );
+    OS_Tls_Handle_t hTls;
 
-    mbedtls_pk_context pkey;
-    mbedtls_pk_init( &pkey );
-
-    mbedtls_debug_set_threshold( DEBUG_LEVEL );
+    err = OS_Tls_init(&hTls, &tlsConfig);
+    if (OS_SUCCESS != err)
+    {
+        Debug_LOG_ERROR("OS_Tls_init() failed, code %d", err);
+        return -1;
+    }
+    Debug_LOG_INFO("TLS library successfully initialized");
 
     // -------------------------------------------------------------------------
-    // Init entropy source
-
-    if_OS_Entropy_t entropy = IF_OS_ENTROPY_ASSIGN(
-                                  entropy_rpc,
-                                  entropy_port);
-
-    // -------------------------------------------------------------------------
-    // Load the certificates and private RSA key
-
-    Debug_LOG_INFO( "  . Loading the server cert. and key..." );
-
-    int ret = mbedtls_x509_crt_parse( &srvcert,
-                                      (const unsigned char*) cTlsServerCert,
-                                      cTlsServerCertLen );
-    if ( ret != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  !  mbedtls_x509_crt_parse returned -0x%04X",
-                         -ret );
-        return -1;
-    }
-
-    ret = mbedtls_x509_crt_parse( &srvcert,
-                                  (const unsigned char*) cTlsServerRootCert,
-                                  cTlsServerRootCertLen );
-    if ( ret != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  !  mbedtls_x509_crt_parse returned -0x%04X",
-                         -ret );
-        return -1;
-    }
-
-    ret = mbedtls_pk_parse_key( &pkey, (const unsigned char*) cTlsServerKey,
-                                cTlsServerKeyLen, NULL, 0 );
-    if ( ret != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  !  mbedtls_pk_parse_key returned -0x%04X", -ret );
-        return -1;
-    }
-
-    Debug_LOG_INFO( "  . ok" );
-
-    // -------------------------------------------------------------------------
-    // Seed the RNG
-
-    Debug_LOG_INFO( "  . Seeding the random number generator..." );
-
-    const char* pers = "ssl_server";
-
-    if ( ( ret = mbedtls_ctr_drbg_seed( &ctr_drbg, entropyWrapper, &entropy,
-                                        (const unsigned char*) pers,
-                                        strlen( pers ) ) ) != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  ! mbedtls_ctr_drbg_seed returned -0x%04X", -ret );
-        return -1;
-    }
-
-    Debug_LOG_INFO( "  . ok" );
-
-    // -------------------------------------------------------------------------
-    // Setup stuff
-
-    Debug_LOG_INFO( "  . Setting up the SSL data...." );
-
-    if ( ( ret = mbedtls_ssl_config_defaults( &conf,
-                                              MBEDTLS_SSL_IS_SERVER,
-                                              MBEDTLS_SSL_TRANSPORT_STREAM,
-                                              MBEDTLS_SSL_PRESET_DEFAULT ) ) != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  ! mbedtls_ssl_config_defaults returned -0x%04X",
-                         -ret );
-        return -1;
-    }
-
-    // Enable client authentication, i.e. client needs to provide a certificate.
-    mbedtls_ssl_conf_authmode( &conf, MBEDTLS_SSL_VERIFY_REQUIRED );
-
-    // Set allowed cipher suites.
-    mbedtls_ssl_conf_ciphersuites( &conf, cCipherSuites );
-
-    mbedtls_ssl_conf_rng( &conf, mbedtls_ctr_drbg_random, &ctr_drbg );
-    mbedtls_ssl_conf_dbg(&conf, logDebug, NULL);
-
-    mbedtls_ssl_conf_ca_chain( &conf, srvcert.next, NULL );
-
-    if ( ( ret = mbedtls_ssl_conf_own_cert( &conf, &srvcert, &pkey ) ) != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  ! mbedtls_ssl_conf_own_cert returned -0x%04X",
-                         -ret );
-        return -1;
-    }
-
-    if ( ( ret = mbedtls_ssl_setup( &ssl, &conf ) ) != 0 )
-    {
-        Debug_LOG_ERROR( "  . failed  ! mbedtls_ssl_setup returned -0x%04X", -ret );
-        return -1;
-    }
-
-    Debug_LOG_INFO( "  . ok" );
 
     for (;;)
     {
-        mbedtls_ssl_session_reset( &ssl );
-
         // ---------------------------------------------------------------------
         // Wait until a client connects
 
-        Debug_LOG_INFO( "  . Waiting for a remote connection ..." );
+        Debug_LOG_INFO( "Waiting for a remote connection..." );
 
-        OS_Socket_Handle_t hSocket;
         OS_Socket_Addr_t srcAddr = {0};
 
         do
@@ -531,7 +294,8 @@ run(void)
             err = waitForIncomingConnection(hServer.handleID);
             if (err != OS_SUCCESS)
             {
-                Debug_LOG_ERROR("waitForIncomingConnection() failed, error %d", err);
+                Debug_LOG_ERROR("waitForIncomingConnection() failed, code % d",
+                                err);
                 goto exit;
             }
 
@@ -544,126 +308,100 @@ run(void)
 
         if (err != OS_SUCCESS)
         {
-            Debug_LOG_ERROR("OS_Socket_accept() failed, error %d", err);
+            Debug_LOG_ERROR("OS_Socket_accept() failed, code %d", err);
             goto exit;
         }
-
-        mbedtls_ssl_set_bio( &ssl, &hSocket, sendFunc, recvFunc, NULL );
-
-        Debug_LOG_INFO( "  . ok" );
 
         // ---------------------------------------------------------------------
         // Handshake
 
-        Debug_LOG_INFO( "  . Performing the SSL/TLS handshake..." );
-
-        while ( ( ret = mbedtls_ssl_handshake( &ssl ) ) != 0 )
+        do
         {
-            if ( ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE )
-            {
-                seL4_Yield(); // Yield and try again next time.
-            }
-            else
-            {
-                Debug_LOG_ERROR( "  . failed  ! mbedtls_ssl_handshake returned -0x%04X", -ret );
-                goto exit;
-            }
+            seL4_Yield();
+            err = OS_Tls_handshake(hTls);
         }
+        while (err == OS_ERROR_WOULD_BLOCK);
 
-        Debug_LOG_INFO( "  . ok" );
+        if (OS_SUCCESS != err)
+        {
+            Debug_LOG_ERROR("OS_Tls_handshake() failed, code %d", err);
+            goto exit;
+        }
+        Debug_LOG_INFO("TLS connection established");
 
         // ---------------------------------------------------------------------
-        // Read the HTTP request
+        // Receive request
 
-        int len;
-        unsigned char buf[1024];
+        static char rxBuf[1024] = {0};
+        size_t rxSize = sizeof(rxBuf);
 
-        Debug_LOG_INFO( "  .  < Read from client:" );
-
-        for (;;)
+        do
         {
-            len = sizeof( buf ) - 1;
-            memset( buf, 0, sizeof( buf ) );
-            ret = mbedtls_ssl_read( &ssl, buf, len );
+            seL4_Yield();
+            err = OS_Tls_read(hTls, rxBuf, &rxSize);
+        }
+        while (err == OS_ERROR_WOULD_BLOCK);
 
-            if ( ret == MBEDTLS_ERR_SSL_WANT_READ )
-            {
-                seL4_Yield(); // Yield and try again next time.
-                continue;
-            }
-
-            if ( ret <= 0 )
-            {
-                switch ( ret )
-                {
-                case MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY:
-                    Debug_LOG_INFO( "  . connection was closed gracefully" );
-                    break;
-
-                default:
-                    Debug_LOG_ERROR( "  . mbedtls_ssl_read returned -0x%04X", -ret );
-                    break;
-                }
-
-                break;
-            }
-
-            len = ret;
-            Debug_LOG_INFO( "  . %d bytes read %s", len, (char*) buf );
-
-            if ( ret > 0 )
-            {
-                break;
-            }
+        switch (err)
+        {
+        case OS_SUCCESS:
+            rxBuf[sizeof(rxBuf) - 1] = '\0'; // ensure buffer is nul-terminated
+            Debug_LOG_INFO("Received %zu bytes:\n%s", rxSize, rxBuf);
+            break;
+        case OS_ERROR_CONNECTION_CLOSED:
+            Debug_LOG_WARNING("OS_Tls_read() connection closed by network "
+                              "stack");
+            break;
+        case OS_ERROR_NETWORK_CONN_SHUTDOWN:
+            Debug_LOG_WARNING("OS_Tls_read() connection reset by peer");
+            break;
+        default:
+            Debug_LOG_ERROR("OS_Tls_read() failed, code %d, bytes read %zu",
+                            err, rxSize);
+            goto exit;
         }
 
         // ---------------------------------------------------------------------
-        // Write the 200 response
+        // Send response
 
-        Debug_LOG_INFO( "  .  > Write to client:" );
+        unsigned char txBuf[] = HTTP_RESPONSE;
+        size_t txSize = sizeof(txBuf);
 
-        len = sprintf( (char*) buf, HTTP_RESPONSE,
-                       mbedtls_ssl_get_ciphersuite( &ssl ) );
-
-        while ( ( ret = mbedtls_ssl_write( &ssl, buf, len ) ) <= 0 )
+        do
         {
-            if ( ret == MBEDTLS_ERR_SSL_WANT_WRITE )
-            {
-                seL4_Yield(); // Yield and try again next time.
-            }
-            else
-            {
-                Debug_LOG_ERROR( "  . failed  ! mbedtls_ssl_write returned -0x%04X", -ret );
-                goto exit;
-            }
+            seL4_Yield();
+            err = OS_Tls_write(hTls, txBuf, &txSize);
+        }
+        while (err == OS_ERROR_WOULD_BLOCK);
+
+        if (OS_SUCCESS != err)
+        {
+            Debug_LOG_ERROR("OS_Tls_write() failed, code %d", err);
+            goto exit;
         }
 
-        len = ret;
-        Debug_LOG_INFO( "  . %d bytes written %s", len, (char*) buf );
+        txBuf[sizeof(txBuf) - 1] = '\0'; // ensure buffer is nul-terminated
+        Debug_LOG_INFO("Sent %zu bytes:\n%s", txSize, txBuf);
 
+exit:
         // ---------------------------------------------------------------------
         // Close connection
 
-        Debug_LOG_INFO( "  . Closing the connection..." );
-
-        while ( ( ret = mbedtls_ssl_close_notify( &ssl ) ) < 0 )
+        err = OS_Tls_reset(hTls);
+        if (OS_SUCCESS != err)
         {
-            if ( ret == MBEDTLS_ERR_SSL_WANT_READ || ret == MBEDTLS_ERR_SSL_WANT_WRITE )
-            {
-                seL4_Yield(); // Yield and try again next time.
-            }
-            else
-            {
-                Debug_LOG_ERROR( "  . failed  ! mbedtls_ssl_close_notify returned -0x%04X",
-                                 -ret );
-                goto exit;
-            }
+            Debug_LOG_ERROR("OS_Tls_reset() failed, code %d", err);
+            goto exit;
         }
 
-exit:
-        OS_Socket_close(hSocket);
+        err = OS_Socket_close(hSocket);
+        if (OS_SUCCESS != err)
+        {
+            Debug_LOG_ERROR("OS_Socket_close() failed, code %d", err);
+            goto exit;
+        }
 
-        Debug_LOG_INFO( "  . ok" );
+        Debug_LOG_INFO("TLS connection closed");
 
         // ---------------------------------------------------------------------
     }
